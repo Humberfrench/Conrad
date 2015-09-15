7d11bcdcf1b0f62b4fd87443f75e873ed87b60d7
Humberto Almeida <humberto.almeida@french.com.br>
2015-09-15 10:06:54 -0300

    zzz1

--- Conrad/App_Start/BundleConfig.cs
+++ Conrad/App_Start/BundleConfig.cs
@@ -6,10 +6,10 @@ namespace Conrad
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/Scripts").Include(
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
                                                 "~/Scripts/bootstrap.min.js",
                                                 "~/Scripts/jquery-2.1.4.min.js"));
-            bundles.Add(new StyleBundle("~/Content/Public/css").Include(
+            bundles.Add(new StyleBundle("~/Content/css").Include(
                                                 "~/Content/bootstrap.min.css",
                                                 "~/Content/bootstrap-theme.min.css",
                                                 "~/Content/Conrad.css"));
