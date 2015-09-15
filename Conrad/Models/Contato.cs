30125288fb273eecd4f0ffcfe191ecdbc637745b
Humberto Almeida <humberto.almeida@french.com.br>
2015-09-15 17:17:54 -0300

    model contato

---
+++ Conrad/Models/Contato.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Conrad.Models
+{
+    public class Contato
+    {
+        public string Nome { get; set; }
+        public string Empresa { get; set; }
+        public string EMail { get; set; }
+        public string RamoAtividade { get; set; }
+        public string DDDTelefone { get; set; }
+        public string Telefone { get; set; }
+        public string DDDCelular { get; set; }
+        public string Celular { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
